9a552f447c704070fbafd03eede9518ec7b8fb45
Paul Smithdale <8188140+mrsmitty@users.noreply.github.com>
2022-03-10 08:06:54 +0800

    WTF gihub

--- .github/workflows/scripts/do-stuff.sh
+++ .github/workflows/scripts/do-stuff.sh
@@ -1,3 +1,8 @@
-export A_VALUE=123asd
+export B_VALUE=456xyz
 
-echo "A_VALUE=${A_VALUE}" >> $GITHUB_ENV
+echo "B_VALUE=${B_VALUE}" >> $GITHUB_ENV
+
+
+echo "A_VALUE=${A_VALUE}"
+echo "B_VALUE=${B_VALUE}"
+echo "A_SECRET=${A_SECRET}" 
